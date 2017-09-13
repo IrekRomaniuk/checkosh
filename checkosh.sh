2e0cfc771f35cbc70f0c65019486b57487913bfb
irom77 <irek.romaniuk@gmail.com>
2017-09-13 12:02:06 -0400

    refactored db.ReadFile

--- checkosh.sh
+++ checkosh.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 cd /var/scripts
-# Skip 3 lines and print lines which do not have "-" in 3rd column and 3rd column does not start from 0. and second column does not start from 10. 
-echo "Writing All Robo to checkosh1 " 
-LSMcli 1.1.1.1 user password Show -F=nibtp | tail -n +4 | awk '{ if ($3 != "-" && $2 !~ /^10\./ && $3 !~ /^0\./) print $0 }' > checkosh1
+# Skip 3 lines and print lines which do not have "-" in 3rd column and it does not start from 0. 
+# and second column does not start from 10. 
+echo "Writing All Robo info including names, external addresses and policy names to checkosh-lsm" 
+LSMcli 1.1.1.1 user password Show -F=nibtp | tail -n +4 | awk '{ if ($3 != "-" && $2 !~ /^10\./ && $3 !~ /^0\./) print $0 }' > checkosh-lsm
+echo "Wrting ALL non-Robo to checkosh-gws"
+LSMcli 10.254.253.237 CPService IHateMundays1! Show -F=nibtp | tail -n +4 | awk '{ if ($3 != "-" && $2 !~ /^10\./ && $2 !~ /^0\./&& $3 !~ /^0\./) print $0 }' > checkosh-gws
 echo "Writing Topology to checkosh-output.sh"
-awk '{if ($2 ~ /^0\./) print "LSMcli 1.1.1.1 user password ShowROBOTopology " $1}' checkosh1 > checkosh-output.sh
-echo "Running checkosh-output.sh with output set to checkosh2"
+awk '{if ($2 ~ /^0\./) print "LSMcli 1.1.1.1 user password ShowROBOTopology " $1}' checkosh-lsm > checkosh-output.sh
 chmod +x checkosh-output.sh 
-./checkosh-output.sh | grep 'ROBO\|1)' > checkosh2
+echo "Executing checkosh-output.sh with output set to intermediate checkosh-int.tmp"
+./checkosh-output.sh | grep 'ROBO\|1)' > checkosh-int.tmp
+echo "Parsing checkosh-int.tmp to extract Robo names, internal addresses and comments into checkosh-int"
+awk '/^ROBO/ { n = $2 } /^1\)/ { $1 = n; $3 = ""; print }' checkosh-int.tmp> checkosh-int
 ##### NOTES
 # PRINT ALL GWs with addresses starting from 10.
 #LSMcli LSMcli 1.1.1.1 user password Show -F=nibtp | awk '{ if ($3 != "-" && $2 ~ /^10\./) print $0 }'
--- db/db.go
+++ db/db.go
@@ -3,12 +3,21 @@ package db
 import (
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
+	"bufio"
+	"os"
+	"strings"
+	//"log"
 )
 // Session to db
 type Session *mgo.Session
 // Robo struct
 type Robo struct {
 	Name, Ext, Int, Policy, DC1, DC2 string
+}
+// File struct
+type File struct {
+	Name string
+	Mapping map[string]int
 }
 // Connect to db
 func Connect(server  string) (*mgo.Session, error) {
@@ -17,10 +26,8 @@ func Connect(server  string) (*mgo.Session, error) {
 	if err != nil {
 		  return nil, err
 	}
-   
 	// Optional. Switch the session to a monotonic behavior.
 	session.SetMode(mgo.Monotonic, true)
-  
 	return session, nil  
   }
 
@@ -30,6 +37,28 @@ func Upsert(robo Robo, session *mgo.Session, db, collection string, Name string)
 	info, err := c.Upsert(bson.M{"name": Name}, robo)
 	return info, err		
   }
+// ReadFile reads file into db
+func ReadFile(address, database, collection string, f File) error {
+	file, err := os.Open(f.Name)	
+	if err != nil { return err }	
+	defer file.Close()
+	session, err := Connect(address)
+	if err != nil { return err }
+	
+	// create a new scanner and read the file line by line
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		robo := strings.Fields(scanner.Text())
+		//log.Println(robo[f.Mapping["Name"]], robo[f.Mapping["Ext"]], robo[len(robo)-1])	
+		r := Robo{Name:robo[f.Mapping["Name"]], Ext:robo[f.Mapping["Ext"]], 
+			Int:robo[f.Mapping["Int"]], Policy:robo[len(robo)-1]}	
+		_, err = Upsert(r, session, database, collection, robo[0])
+		if err != nil { return err }
+	}
+	err = scanner.Err() 
+	return err	  
+}
+
 // Insert inserts document into collection
  func Insert(robo Robo, session *mgo.Session, db, collection string) error {
 	c := session.DB(db).C(collection)
